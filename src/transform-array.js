68cd1cb8ccc40023772dd5e242a94b9e2d3f8e06
Egor <79473900+Vooodee@users.noreply.github.com>
2021-03-31 00:23:24 +0300

    feat: add update

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
+// transform
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(  arr ) {
+ try { 
+  if (Array.isArray(arr) !== true){
+     throw new RangeError();
+  }
+}
+    catch (e) {
+  alert( "Извините, в данных ошибка. Число arr = " + arr + " должно являться массивом");
+}
+console.log(arr);
+let itog = [];
+for( let i = 0;i < arr.length; i++ ){
+    if (typeof  arr[i] === "number"){
+      itog.push(arr[i]);
+    } else if (arr[i] === "--double-next" ){
+      if (typeof  arr[i+1] === "number"){
+         itog.push(arr[i+1]);
+      }
+    } else if (arr[i] === "--double-prev" ){
+      if (typeof  arr[i-1] === "number"){
+          itog.push(arr[i-1]);
+      }
+    } else if (arr[i] === "--discard-next" ){
+        let x = arr.indexOf(arr[i]) 
+          console.log(x); 
+          arr.splice((x), 1);
+    } else if (arr[i] === "--discard-prev" ){
+      let x = arr.indexOf(arr[i]) 
+          console.log(x); 
+        itog.pop();
+    }
+}
+return itog
 };
